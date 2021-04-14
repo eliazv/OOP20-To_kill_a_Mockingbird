83a956561d09abf8d48d04e4728c98cd234b520b
PietroLelli <pietro.lelli2@studio.unibo.it>
2021-04-14 17:12:22 +0200

    add gamestructure

--- src/gamestructure/mainmenu/MainMenuControllerImpl.java
+++ src/gamestructure/mainmenu/MainMenuControllerImpl.java
@@ -2,4 +2,16 @@ package gamestructure.mainmenu;
 
 public class MainMenuControllerImpl implements MainMenuController {
 
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
