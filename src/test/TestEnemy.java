a34bbb32ae438e179e1bbfddf4f334425d6506eb
“eliazavatta” <eliazavatta@studio.unibo.it>
2021-06-24 14:07:25 +0200

    little changes

--- src/test/TestEnemy.java
+++ src/test/TestEnemy.java
@@ -6,47 +6,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import model.enemy.VehicleImpl;
 import model.enemy.VehicleType;
 
-
 public class TestEnemy {
-	
-	private static final int VEHICLE_SCROLL = 1;
-	private static final int CAR_SPEED = 2;
-	private static final int CAMION_SPEED = 1;
-	private static final int TRAIN_SPEED = 10;
-	private static final int RANDOM_SPEED_ADDITION = 10;
-	private static final int RANDOM_XLOCSPAWN_ADDITION = 1500;
 
-	
-	/**
+    /**
+     * constants for vehicles
+     */
+    private static final int VEHICLE_SCROLL = 1;
+    private static final int CAR_SPEED = 2;
+    private static final int TRUCK_SPEED = 1;
+    private static final int TRAIN_SPEED = 10;
+    private static final int RANDOM_SPEED_ADDITION = 10;
+    private static final int RANDOM_XLOCSPAWN_ADDITION = 1500;
+
+    /**
      * This test verify the correct creation and working of the car.
      */
     @org.junit.Test
     public void testCar() {
-    	VehicleImpl vehicle = new VehicleImpl();
-    	vehicle.setCar(4);
-    	assertEquals(4, vehicle.getVehicle().getYLoc());
-    	assertEquals(VEHICLE_SCROLL, vehicle.getVehicle().getYDir());
-    	assertTrue(vehicle.getVehicleType()==VehicleType.TRUCK || vehicle.getVehicleType()==VehicleType.CAR);
-    	assertTrue(vehicle.getVehicleSpeed()>= CAMION_SPEED &&  vehicle.getVehicleSpeed() <= CAR_SPEED + RANDOM_SPEED_ADDITION );
-    	assertTrue(vehicle.getVehicleXLocSpawn() >= CAMION_SPEED && vehicle.getVehicleXLocSpawn() <= CAR_SPEED * RANDOM_XLOCSPAWN_ADDITION );
+        VehicleImpl vehicle = new VehicleImpl();
+        vehicle.setCar(4);
+        assertEquals(4, vehicle.getVehicle().getYLoc());
+        assertEquals(VEHICLE_SCROLL, vehicle.getVehicle().getYDir());
+        assertTrue(vehicle.getVehicleType() == VehicleType.TRUCK || vehicle.getVehicleType() == VehicleType.CAR);
+        assertTrue(vehicle.getVehicleSpeed() >= TRUCK_SPEED
+                && vehicle.getVehicleSpeed() <= CAR_SPEED + RANDOM_SPEED_ADDITION);
+        assertTrue(vehicle.getVehicleXLocSpawn() >= TRUCK_SPEED
+                && vehicle.getVehicleXLocSpawn() <= CAR_SPEED * RANDOM_XLOCSPAWN_ADDITION);
     }
 
-    
-
-	/**
+    /**
      * This test verify the correct creation and working of the train.
      */
     @org.junit.Test
     public void testTrain() {
-    	VehicleImpl vehicle = new VehicleImpl();
-    	vehicle.setTrain(4);
-    	assertEquals(4, vehicle.getVehicle().getYLoc());
-    	assertEquals(VEHICLE_SCROLL, vehicle.getVehicle().getYDir());
-    	assertEquals(VehicleType.TRAIN, vehicle.getVehicleType());
-    	assertTrue(vehicle.getVehicleSpeed()>= TRAIN_SPEED &&  vehicle.getVehicleSpeed() <= TRAIN_SPEED + RANDOM_SPEED_ADDITION );
-    	assertTrue(vehicle.getVehicleXLocSpawn() >= TRAIN_SPEED && vehicle.getVehicleXLocSpawn() <= TRAIN_SPEED * RANDOM_XLOCSPAWN_ADDITION );
+        VehicleImpl vehicle = new VehicleImpl();
+        vehicle.setTrain(4);
+        assertEquals(4, vehicle.getVehicle().getYLoc());
+        assertEquals(VEHICLE_SCROLL, vehicle.getVehicle().getYDir());
+        assertEquals(VehicleType.TRAIN, vehicle.getVehicleType());
+        assertTrue(vehicle.getVehicleSpeed() >= TRAIN_SPEED
+                && vehicle.getVehicleSpeed() <= TRAIN_SPEED + RANDOM_SPEED_ADDITION);
+        assertTrue(vehicle.getVehicleXLocSpawn() >= TRAIN_SPEED
+                && vehicle.getVehicleXLocSpawn() <= TRAIN_SPEED * RANDOM_XLOCSPAWN_ADDITION);
     }
-    
-
 
 }
--- src/test/TestStrip.java
+++ src/test/TestStrip.java
@@ -7,30 +7,29 @@ import model.map.Strip;
 import model.map.StripEnvironment;
 import model.map.StripImpl;
 
-public class TestStrip {	
-	
-	/**
+public class TestStrip {
+
+    /**
      * This test verify the correct creation and working of the Strip.
      */
     @org.junit.Test
     public void testStrip() {
-    	
-    	Strip strip = new StripImpl();
-    	
-    	strip.getRndStrip(6);
-    	assertEquals(8, strip.getStrip().size());
-    	
-    	strip.getSpecificStrip("Grass.png", "Tree.png", 2);
-    	assertEquals(8, strip.getStrip().size());
-    	assertEquals(StripEnvironment.GRASS, strip.getStripEnvironment());
-    	assertTrue(strip.getTreeNumber() <= 8 && strip.getTreeNumber() >= 0 );
-    	
-    	strip.getSpecificStrip("Rail.png", 5);
-    	assertEquals(8, strip.getStrip().size());
-    	assertEquals("Rail.png", strip.getBoxOfStrip(2).getImage().getFileName());
-    	assertEquals(StripEnvironment.RAIL, strip.getStripEnvironment());
-    	
-    	
-    } 
-      
+
+        Strip strip = new StripImpl();
+
+        strip.getRndStrip(6);
+        assertEquals(8, strip.getStrip().size());
+
+        strip.getSpecificStrip("Grass.png", "Tree.png", 2);
+        assertEquals(8, strip.getStrip().size());
+        assertEquals(StripEnvironment.GRASS, strip.getStripEnvironment());
+        assertTrue(strip.getTreeNumber() <= 8 && strip.getTreeNumber() >= 0);
+
+        strip.getSpecificStrip("Rail.png", 5);
+        assertEquals(8, strip.getStrip().size());
+        assertEquals("Rail.png", strip.getBoxOfStrip(2).getImage().getFileName());
+        assertEquals(StripEnvironment.RAIL, strip.getStripEnvironment());
+
+    }
+
 }
